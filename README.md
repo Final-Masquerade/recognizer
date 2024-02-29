61189673fc5ea43afa553d012ec6328599a0636c
XenoverseUp <xenoverseup@gmail.com>
2024-02-29 23:52:08 +0300

    init: setup project

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 An ML application server written in Python that processes the request and sends response in MusicXML. It uses Flask to build API.
 
 ## Commands
-- `flask run`: Starts the development server. Make sure you activate the virtual env first to run this command.
+- `make start`: Starts the development server. Make sure you activate the virtual env first to run this command.
--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-from flask import Flask
+import os
+from dotenv import load_dotenv
+from flask import Flask, request, send_file
+import uuid
+
+load_dotenv() # os.getenv("SECRET_KEY")
 
 app = Flask(__name__)
 
-
 @app.get("/")
 def index():
-    return "Hello world."
+    return "Stable."
+
+@app.post("/api/recognize")
+def recognize():
+    data = request.get_json()
+
+    return "Recognized."
+    # return send_file('/temp/0000-0000-0000-0000.musicxml')
+
+@app.post("/api/harmonize")
+def harmonize():
+    data = request.get_json()
+
+    return "Harmonized."
